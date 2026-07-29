fbf17a577b4c6546a2cfcf6a0d6c077b242c823d
Siva <sivadaggolu3@gmail,com>
2026-07-29 15:39:58 +0530

    intial commit

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,12 +1,14 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
+  const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
   return config;
 });
 
